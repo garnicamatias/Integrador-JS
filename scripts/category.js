6e9216d2f853fe9b8198914fb886c44f4a4c8719
garnicamatias <garnicamatias02@gmail.com>
2022-11-24 02:37:33 -0300

    sort options added

--- scripts/category.js
+++ scripts/category.js
@@ -1,6 +1,8 @@
 const urlTitle = document.getElementsByTagName('title')
 const subtitleContainer = document.querySelector('.subtitleContainer')
 const showMoreBtn = document.querySelector('.showMoreBtn')
+const sortAscBtn = document.querySelector('.sortAscBtn')
+const sortDescBtn = document.querySelector('.sortDescBtn')
 
 const values = window.location.search;
 const urlParams = new URLSearchParams(values);
@@ -44,7 +46,46 @@ const renderCategoryProducts = async (e) => {
 	console.log(categoryProductsArray)
 };
 
+let newArray=[]
+
+const renderCardSync = (data) => {
+
+	const {id,  title: name, pictures: img,  price} = data;
+	return `
+	<div class="itemCard">
+		<div class="favIconContainer">
+			<img src="/assets/img/favHeart.png" alt="agregar a favoritos">
+		</div>
+		<div class="imgCardContainer">
+		   <img src="${img[0].secure_url}" alt="">
+		</div>
+		<div class="cardDataContainer">
+			<p> ${name.slice(0,40)}</p>
+			<p>$${Math.trunc(price)}</p>
+		</div>
+		<button class="addToCart" data-id="${id}">Añadir al Carrito</button>
+	</div>`
+}
+
+const sortPriceAsc = (e) =>{
+	let newArray = categoryProductsArray
+	newArray.sort((a,b)=>{return a.price - b.price})
+	categorySelected.innerHTML =""
+	newArray.forEach(element => categorySelected.innerHTML += renderCardSync(element))
+}
+
+const sortPriceDesc = (e) =>{
+	let newArray = categoryProductsArray
+	newArray.sort((a,b)=>{return b.price - a.price})
+	categorySelected.innerHTML =""
+	newArray.forEach(element => categorySelected.innerHTML += renderCardSync(element))
+}
+
 renderCategoryName(categoryId, urlTitle[0].lastChild)
 renderCategoryName(categoryId, subtitleContainer.children[0])
 renderCategoryProducts()
 showMoreBtn.addEventListener("click", renderCategoryProducts)
+// showMoreBtn.addEventListener("click", sortAscendenting(categoryProductsArray))
+categorySelectedContainer.addEventListener("click", addToCart);
+sortAscBtn.addEventListener("click", sortPriceAsc)
+sortDescBtn.addEventListener("click", sortPriceDesc)
